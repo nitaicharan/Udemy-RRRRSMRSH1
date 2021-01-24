2d8a6bd073639f60f82f3f1025bdd08958264c08
Nitai Charan <nitaicharan@gmail.com>
2021-01-24 00:00:00 -0300

    75. Splitting our Backend to have two endpoints to use with Fork

--- src/saga/index.js
+++ src/saga/index.js
@@ -1,7 +1,7 @@
-import * as testSaga from "./testeSaga";
-// import * as entriesSaga from "./entries.saga";
+// import * as testSaga from "./testeSaga";
+import * as entriesSaga from "./entries.saga";
 
 export function initSagas(sagaMiddleware) {
-    Object.values(testSaga).forEach(sagaMiddleware.run.bind(sagaMiddleware));
-    // Object.values(entriesSaga).forEach(sagaMiddleware.run.bind(sagaMiddleware));
+    // Object.values(testSaga).forEach(sagaMiddleware.run.bind(sagaMiddleware));
+    Object.values(entriesSaga).forEach(sagaMiddleware.run.bind(sagaMiddleware));
 }
