9b5dace35b45f1b7ab220925e80a9fb5828b6c66
Nitai Charan <nitaicharan@gmail.com>
2021-01-24 00:00:00 -0300

    74. Understand another Saga Effect FORK

--- src/saga/testeSaga.js
+++ src/saga/testeSaga.js
@@ -1,4 +1,4 @@
-import { call, delay, put, take } from "redux-saga/effects";
+import { call, delay, fork, put, take } from "redux-saga/effects";
 
 function double(number) {
     return number * 2;
@@ -13,16 +13,32 @@ export function* testSaga() {
         console.log(a);
         const b = yield call(double, 3);
         console.log(b);
-        
+
         console.log("Finish saga function");
 
         console.log(store);
     }
 }
 
+export function* doNothing() {
+    console.log("I'm have been called");
+    yield delay(500);
+    console.log("I'm doing nothing");
+}
+
+export function* testSagaFork() {
+    while (true) {
+        yield take('TEST_MESSAGE_2')
+        yield delay(5000);
+        yield fork(doNothing);
+        yield fork(doNothing);
+        yield fork(doNothing);
+    }
+}
+
 export function* dispatchTest() {
     while (true) {
         yield delay(1000);
-        yield put({ type: 'TEST_MESSAGE' })
+        yield put({ type: 'TEST_MESSAGE_2' })
     }
 }
