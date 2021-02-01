8ecfad0d0821a49dad010c8e47e2c433164c4066
Nitai Charan <nitaicharan@gmail.com>
2021-02-01 00:00:00 -0300

    50. Dispatching new actions to the new Redux Store

--- src/components/EntryLine.js
+++ src/components/EntryLine.js
@@ -2,6 +2,7 @@ import React, { Fragment } from 'react';
 import { useDispatch } from 'react-redux';
 import { Grid, Icon, Segment } from 'semantic-ui-react';
 import { removeEntryRedux } from './../actions/entries.action';
+import { openEditModal } from './../actions/modals.action';
 
 function EntryLine({ id, description, value, isExpense = false, editEntry }) {
     const dispatch = useDispatch();
@@ -14,7 +15,7 @@ function EntryLine({ id, description, value, isExpense = false, editEntry }) {
                         <Grid.Column width={10} textAlign='left'>{description}</Grid.Column>
                         <Grid.Column width={3} textAlign='right'>{value}</Grid.Column>
                         <Grid.Column width={3}>
-                            <Icon name='edit' onClick={() => editEntry(id)} />
+                            <Icon name='edit' onClick={() => dispatch(openEditModal(id))} />
                             <Icon name='trash' onClick={() => dispatch(removeEntryRedux(id))} />
                         </Grid.Column>
                     </Grid.Row>
--- src/reducers/dist/modals.reducer.dev.js
+++ src/reducers/dist/modals.reducer.dev.js
@@ -5,12 +5,25 @@ Object.defineProperty(exports, "__esModule", {
 });
 exports["default"] = void 0;
 
+function ownKeys(object, enumerableOnly) { var keys = Object.keys(object); if (Object.getOwnPropertySymbols) { var symbols = Object.getOwnPropertySymbols(object); if (enumerableOnly) symbols = symbols.filter(function (sym) { return Object.getOwnPropertyDescriptor(object, sym).enumerable; }); keys.push.apply(keys, symbols); } return keys; }
+
+function _objectSpread(target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i] != null ? arguments[i] : {}; if (i % 2) { ownKeys(source, true).forEach(function (key) { _defineProperty(target, key, source[key]); }); } else if (Object.getOwnPropertyDescriptors) { Object.defineProperties(target, Object.getOwnPropertyDescriptors(source)); } else { ownKeys(source).forEach(function (key) { Object.defineProperty(target, key, Object.getOwnPropertyDescriptor(source, key)); }); } } return target; }
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
+
 var reducer = function reducer() {
-  var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
-  var payload = arguments.length > 1 ? arguments[1] : undefined;
+  var state = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {
+    isOpen: false
+  };
+  var action = arguments.length > 1 ? arguments[1] : undefined;
 
   switch (key) {
     case 'OPEN_EDIT_MODAL':
+      return _objectSpread({}, state, {
+        isOpen: true,
+        id: action.payload.id
+      });
+
     case 'CLOSE_EDIT_MODAL':
     default:
       return state;
