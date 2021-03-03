9e4466e679f38a7465dd3362d9fd73de91e83a39
Nitai Charan <nitaicharan@gmail.com>
2021-03-02 22:01:37 -0300

    4. Creating our income and expense history

--- src/App.js
+++ src/App.js
@@ -1,33 +1,77 @@
-import { Container, Grid, Header, Segment, Statistic } from 'semantic-ui-react'
+import { Container, Grid, Header, Icon, Segment, Statistic } from 'semantic-ui-react';
 import './App.css';
 
 function App() {
   return (
     <Container>
       <Header as='h1'></Header>
+
       <Statistic size='small'>
         <Statistic.Label>Your Balance:</Statistic.Label>
         <Statistic.Value>2,550.53</Statistic.Value>
       </Statistic>
+
       <Segment textAlign="center">
         <Grid columns={2} divided>
           <Grid.Row>
             <Grid.Column>
               <Statistic size='tiny' color="green">
-                <Statistic.Label style={{ textAlign: 'left' }}>Your Balance:</Statistic.Label>
+                <Statistic.Label style={{ textAlign: 'left' }}>Income:</Statistic.Label>
                 <Statistic.Value>2,550.53</Statistic.Value>
               </Statistic>
             </Grid.Column>
 
             <Grid.Column>
               <Statistic size='tiny' color="red">
-                <Statistic.Label style={{ textAlign: 'left' }}>Your Balance:</Statistic.Label>
+                <Statistic.Label style={{ textAlign: 'left' }}>Expenses:</Statistic.Label>
                 <Statistic.Value>623.50</Statistic.Value>
               </Statistic>
             </Grid.Column>
           </Grid.Row>
         </Grid>
       </Segment>
+
+      <Header as='H3'>History</Header>
+
+      <Segment color='red'>
+        <Grid columns={3} textAlign="right">
+          <Grid.Row>
+            <Grid.Column width={10} textAlign='left'>Something</Grid.Column>
+            <Grid.Column width={3} textAlign='right'>$10,00</Grid.Column>
+            <Grid.Column width={3}>
+              <Icon name='edit' />
+              <Icon name='trash' />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+
+      <Segment color='green'>
+        <Grid columns={3} textAlign="right">
+          <Grid.Row>
+            <Grid.Column width={10} textAlign='left'>Something else</Grid.Column>
+            <Grid.Column width={3} textAlign='right'>$100,00</Grid.Column>
+            <Grid.Column width={3}>
+              <Icon name='edit' />
+              <Icon name='trash' />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+
+      <Segment color='red'>
+        <Grid columns={3} textAlign="right">
+          <Grid.Row>
+            <Grid.Column width={10} textAlign='left'>Something</Grid.Column>
+            <Grid.Column width={3} textAlign='right'>$20,00</Grid.Column>
+            <Grid.Column width={3}>
+              <Icon name='edit' />
+              <Icon name='trash' />
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+      </Segment>
+      
     </Container>
   );
 }
