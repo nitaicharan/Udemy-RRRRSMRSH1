2616d42b92f7043b305a806e626101ed96951291
Nitai Charan <nitaicharan@gmail.com>
2021-03-04 20:12:53 -0300

    12. Creating our EntryLine React Component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { Container, Grid, Icon, Segment } from 'semantic-ui-react';
+import { Container } from 'semantic-ui-react';
 import './App.css';
 import DisplayBalance from './components/DisplayBalance';
 import DisplayBalances from './components/DisplayBalances';
+import EntryLine from './components/EntryLine';
 import MainHeader from './components/MainHeader';
 import NewEntryForm from './components/NewEntryForm';
 
@@ -16,44 +17,8 @@ function App() {
 
       <MainHeader title='History' type='h3' />
 
-      <Segment color='red'>
-        <Grid columns={3} textAlign="right">
-          <Grid.Row>
-            <Grid.Column width={10} textAlign='left'>Something</Grid.Column>
-            <Grid.Column width={3} textAlign='right'>$10,00</Grid.Column>
-            <Grid.Column width={3}>
-              <Icon name='edit' />
-              <Icon name='trash' />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
-
-      <Segment color='green'>
-        <Grid columns={3} textAlign="right">
-          <Grid.Row>
-            <Grid.Column width={10} textAlign='left'>Something else</Grid.Column>
-            <Grid.Column width={3} textAlign='right'>$100,00</Grid.Column>
-            <Grid.Column width={3}>
-              <Icon name='edit' />
-              <Icon name='trash' />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
-
-      <Segment color='red'>
-        <Grid columns={3} textAlign="right">
-          <Grid.Row>
-            <Grid.Column width={10} textAlign='left'>Something</Grid.Column>
-            <Grid.Column width={3} textAlign='right'>$20,00</Grid.Column>
-            <Grid.Column width={3}>
-              <Icon name='edit' />
-              <Icon name='trash' />
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Segment>
+      <EntryLine description='income' value='$10.00' isExpense />
+      <EntryLine description='expense' value='$10.00' />
 
       <MainHeader title='Add new transaction' type='h3' />
 
