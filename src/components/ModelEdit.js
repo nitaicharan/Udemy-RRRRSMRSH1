46ad9f49af94708c875d05e0c27b40e59e660872
Nitai Charan <nitaicharan@gmail.com>
2021-03-05 19:41:49 -0300

    22. Creating a model to edit the entries1

---
+++ src/components/ModelEdit.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Modal } from 'semantic-ui-react'
+
+function ModelEdit({ isOpen }) {
+    return (
+        <Modal open={isOpen}>
+        </Modal>
+    )
+}
+
+export default ModelEdit
--- src/components/NewEntryForm.js
+++ src/components/NewEntryForm.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import { Form } from 'semantic-ui-react';
+import { Checkbox, Form, Segment } from 'semantic-ui-react';
 import ButtonSaveOrCancel from './ButtonSaveOrCancel';
 
 function NewEntryForm({ addEntry }) {
   const [value, setValue] = useState('');
   const [description, setDescription] = useState('');
+  const [isExpense, setIsExpense] = useState(false)
 
   return (
     <Form unstackable>
@@ -13,6 +14,10 @@ function NewEntryForm({ addEntry }) {
         <Form.Input icon='tags' label='Description' width={12} planceholder='New shinny thing' onChange={(event) => setDescription(event.target.value)} value={description} />
         <Form.Input width={4} label='value' planceholder='100.00' icon='dollar' iconPosition='left' onChange={(event) => setValue(event.target.value)} value={value}></Form.Input>
       </Form.Group>
+
+      <Segment compact>
+        <Checkbox toggle label='is expense' checked={isExpense} onChange={() => setIsExpense(oldState=> !oldState)} />
+      </Segment>
 
       <ButtonSaveOrCancel addEntry={addEntry} description={description} value={value} />
     </Form>
