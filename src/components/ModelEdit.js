016ce01b5c63bbf52aad27bddf88caf3c0a6f55a
Nitai Charan <nitaicharan@gmail.com>
2021-03-05 20:17:09 -0300

    23. Formatting our modal

--- src/components/ModelEdit.js
+++ src/components/ModelEdit.js
@@ -1,9 +1,17 @@
 import React from 'react'
-import { Modal } from 'semantic-ui-react'
+import { Button, Modal } from 'semantic-ui-react'
+import NewEntryForm from './NewEntryForm'
 
-function ModelEdit({ isOpen }) {
+function ModelEdit({ isOpen, setIsOpen }) {
     return (
         <Modal open={isOpen}>
+            <Modal.Header>Edit entry</Modal.Header>
+            <Modal.Content>
+                <NewEntryForm />
+            </Modal.Content>
+            <Modal.Actions>
+                <Button onClick={()=> setIsOpen(false)}></Button>
+            </Modal.Actions>
         </Modal>
     )
 }
