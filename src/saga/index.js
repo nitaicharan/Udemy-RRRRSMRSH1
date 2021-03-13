80ef21f8c1219dfd3ae95f36da63ba35e66d2614
Nitai Charan <nitaicharan@gmail.com>
2021-03-13 00:00:00 -0300

    85. Importing our Add Entries Saga to our Saga Initializer

--- src/saga/index.js
+++ src/saga/index.js
@@ -1,9 +1,11 @@
 // import * as testSaga from "./test.saga";
-import * as entriesSagaDeletion from "../saga/entries-deletion.saga";
+import * as entriesSagaDeletion from "../saga/deletion-entries.saga";
+import * as entriesSagaAdd from "./add-entries.saga";
 import * as entriesSaga from "./entries.saga";
 
 export function initSagas(sagaMiddleware) {
     // Object.values(testSaga).forEach(sagaMiddleware.run.bind(sagaMiddleware));
     Object.values(entriesSaga).forEach(sagaMiddleware.run.bind(sagaMiddleware));
     Object.values(entriesSagaDeletion).forEach(sagaMiddleware.run.bind(sagaMiddleware));
+    Object.values(entriesSagaAdd).forEach(sagaMiddleware.run.bind(sagaMiddleware));
 }
